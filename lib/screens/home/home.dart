f18707e061fd4bb80cdb95e5ea17d4d5b1676b4b
nathan hanif <79506660+nathanhanif@users.noreply.github.com>
2021-05-21 06:23:50 +0300

    database update
    1st db

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,44 +1,73 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_list.dart';
+import 'package:brew_crew/screens/home/settings_form.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      (Column(children: [
-        SizedBox(height: 60.0),
-        Container(
-          height: 692.0,
-          width: 600,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/data.png"),
-              fit: BoxFit.cover,
+    void _showSettingsPanel() {
+      showModalBottomSheet<void>(
+          context: context,
+          builder: (context) {
+            return Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+                child: SettingsForm());
+          });
+    }
+
+    ;
+
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Stack(children: [
+        (Column(children: [
+          SizedBox(height: 60.0),
+          Container(
+            height: 692.0,
+            width: 600,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/data.png"),
+                fit: BoxFit.cover,
+              ),
             ),
+          )
+        ])),
+        Scaffold(
+          resizeToAvoidBottomPadding: false,
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            //title: Text(
+            //  "upsell",
+            //  style: TextStyle(color: Colors.black),
+            // ),
+            backgroundColor: Colors.white,
+            actions: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                label: Text("logout"),
+                padding: EdgeInsets.only(right: 145),
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+              ),
+              FlatButton.icon(
+                icon: Icon(Icons.settings),
+                label: Text("Update Profile"),
+                padding: EdgeInsets.only(right: 10),
+                onPressed: () => _showSettingsPanel(),
+              ),
+            ],
           ),
-        )
-      ])),
-      Scaffold(
-        resizeToAvoidBottomPadding: false,
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text(
-            "upsell",
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.white,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text("logout"),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            )
-          ],
+          body: BrewList(),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
